348ad312cf0a8bcf83c02d12c6f22ec6abc57d94
aarjones <aarjones@clarkson.edu>
2021-12-07 20:35:10 -0500

    Implemented FinCalc

--- src/main/FinCalc.java
+++ src/main/FinCalc.java
@@ -13,6 +13,38 @@ public class FinCalc {
         clearAll();
     }
 
+    public float computeCompound() {
+        return this.principal * (1 + this.interestRate * this.duration);
+    }
+
+    public float computeSimple() {
+        return this.principal * (float) Math.pow(1 + this.interestRate, this.duration);
+    }
+
+    public float getInterestRate() {
+        return this.interestRate;
+    }
+
+    public float getPrincipal() {
+        return this.principal;
+    }
+
+    public float getDuration() {
+        return this.interestRate;
+    }
+
+    public void setInterestRate(float newRate) {
+        this.interestRate = newRate;
+    }
+
+    public void setPrincipal(float newPrinciple) {
+        this.principal = newPrinciple;
+    }
+
+    public void setDuration(float newDuration) {
+        this.duration = newDuration;
+    }
+
     public void clearAll() {
         this.interestRate = DEFAULT_INTEREST_RATE;
         this.principal = DEFAULT_PRINCIPAL;
