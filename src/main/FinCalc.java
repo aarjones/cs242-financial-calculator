8db6735733d9a6403d5b8357100ac1cd0bf2e2dd
aarjones <aarjones@clarkson.edu>
2021-12-07 19:40:13 -0500

    Added framework for FinCalc.java

--- src/main/FinCalc.java
+++ src/main/FinCalc.java
@@ -1,5 +1,21 @@
 package main;
 
 public class FinCalc {
-    
+    public final static int DEFAULT_INTEREST_RATE = 3;
+    public final static int DEFAULT_PRINCIPAL = 1000;
+    public final static int DEFAULT_DURATION = 12;
+
+    private float interestRate;
+    private float principal;
+    private float duration;
+
+    public FinCalc() {
+        clearAll();
+    }
+
+    public void clearAll() {
+        this.interestRate = DEFAULT_INTEREST_RATE;
+        this.principal = DEFAULT_PRINCIPAL;
+        this.duration = DEFAULT_DURATION;
+    }
 }
