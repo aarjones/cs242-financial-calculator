6ad5ae51b482dd1947facc292988545879f51d5b
Matthew Sprague <spragumc@clarkson.edu>
2021-12-13 17:07:03 -0500

    Jar built

---
+++ src/main/Launcher.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Launcher {
+
+    /**
+     * Launch the financial calculator GUI.
+     */
+    public static void main(String [] args)
+    {
+        FinCalcGUI.main(args);
+    }
+
+}
